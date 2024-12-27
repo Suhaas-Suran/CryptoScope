770abdf486fb83dac0bc4fa754f1d0a553e23962
Suhaas <suhaassuran@gmail.com>
2024-12-27 15:45:01 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Navbar from './components/Navbar/navbar'
+import Navbar from './components/Navbar/Navbar'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Coin from './pages/Coin/Coin'
